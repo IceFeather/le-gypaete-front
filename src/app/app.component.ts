80cacf8b1e1353334b0aee951a9043e37556d4d3
IceFeather <benjamin.marlier@gmail.com>
2020-07-22 20:35:44 +0200

    revue structure du site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('afficherMasquer', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(500, style({ opacity: 0 }))
+      ]),
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'Chalet le Gypaete';
@@ -13,6 +26,8 @@ export class AppComponent implements OnInit {
   saison = 'hiver';
 
   isMobile: boolean;
+  top: boolean = true;
+  bottom: boolean = false;
 
   constructor(
     private breakpointObserver: BreakpointObserver
@@ -32,6 +47,19 @@ export class AppComponent implements OnInit {
       this.saison = 'été';
     }
     // this.diaporamaFond.reset();
+  }
+
+  scroll(event: any) {
+    if (event.target.scrollTop === 0) {
+      this.top = true;
+    } else {
+      this.top = false;
+    }
+    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
+      this.bottom = true;
+    } else {
+      this.bottom = false;
+    }
   }
 
 }
